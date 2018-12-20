be4906a34cba113b34c1c7e9131376385d0c41b2
Sharifah Munira <shifa@myorange.my>
2018-12-20 16:56:47 +0800

    manage to create signup logic

--- accounts/views.py
+++ accounts/views.py
@@ -4,17 +4,19 @@ from django.contrib import auth
 
 def signup(request):
     if request.method == 'POST':
-        #user has info and want to sign up
+        # User has info and wants an account now!
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.get(username=request.POST['username'])
-                return render(request, 'accounts/signup.html', {'error':'Username has been taken'})
-            except User.DoestNotExist:
+                return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
+            except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
                 auth.login(request,user)
                 return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
     else:
-        #user wants to enter info
+        # User wants to enter info
         return render(request, 'accounts/signup.html')
 
 def login(request):
