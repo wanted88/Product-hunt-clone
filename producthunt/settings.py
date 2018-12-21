4f5f739798d7b95c906dc2ac9f127317fb5a7a81
Sharifah Munira <shifa@myorange.my>
2018-12-21 12:55:55 +0800

    create products

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'producthunt/static/')
 ]
 
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
